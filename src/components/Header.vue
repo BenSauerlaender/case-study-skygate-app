8970f128fa4e1bae76b2739aac6ef042ee9d3a55
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-05-30 10:35:00 +0200

    WIP

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div>
+      <h1 class="green">Case-Study-Skygate-App</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header div {
+  width: 100%;
+  background-color: blue;
+  top: 0px;
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
