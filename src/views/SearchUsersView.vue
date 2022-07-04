2dab1245a30ad3ce7e4ea27a83d1f31dac97e8fa
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-07-04 11:08:35 +0200

    Add permission failure to the ui of userSearch

--- src/views/SearchUsersView.vue
+++ src/views/SearchUsersView.vue
@@ -2,6 +2,11 @@
 import { useSearchQuery } from "@/helper/searchQuery";
 import SearchUsersInputs from "@/components/SearchUsersInputs.vue";
 import SearchUsersResult from "@/components/SearchUsersResult.vue";
+import { useStore } from "@/stores/store";
+
+const store = useStore();
+
+const hasPermission = store.hasPermission("getAllUsers");
 
 //parse the query from url and ensures the query is valid
 const { query } = useSearchQuery();
@@ -11,13 +16,16 @@ const { query } = useSearchQuery();
 <template>
   <h1 class="heading">{{ $t("sites.search.name") }}</h1>
   <br />
-  <!-- renders as soon as the query is validated  -->
-  <template v-if="query !== null">
+  <!-- renders as soon as the query is validated and the user has the permission -->
+  <template v-if="hasPermission && query !== null">
     <!-- input fields to change the search query  -->
     <SearchUsersInputs :default-query="query!" />
 
     <!-- renders the results -->
     <SearchUsersResult :query="query!" />
+  </template>
+  <template v-else>
+    {{ $t("sites.search.messages.noPermission") }}
   </template>
 </template>
 
